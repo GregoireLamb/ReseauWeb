a255aa883315a78df13eab354352ce1c60cb8705
gregoire de lambertye <gdelambertye03@gmail.com>
2021-11-17 10:20:57 +0100

    initial commit

---
+++ src/http/client/WebPing.java
@@ -0,0 +1,38 @@
+package http.client;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class WebPing {
+  public static void main(String[] args) {
+  
+      if (args.length != 2) {
+      	System.err.println("Usage java WebPing <server host name> <server port number>");
+      	return;
+      }	
+  
+  String httpServerHost = args[0];
+  int httpServerPort = Integer.parseInt(args[1]);
+  httpServerHost = args[0];
+  httpServerPort = Integer.parseInt(args[1]);
+
+    try {
+      InetAddress addr;
+      Socket sock = new Socket(httpServerHost, httpServerPort);
+      addr = sock.getInetAddress();
+      System.out.println("Connected to " + addr);
+
+      //main client
+
+      PrintStream socketOut = new PrintStream(sock.getOutputStream());;
+      PrintWriter serveur = new PrintWriter(sock.getOutputStream());
+
+      sock.close();
+    } catch (java.io.IOException e) {
+      System.out.println("Can't connect to " + httpServerHost + ":" + httpServerPort);
+      System.out.println(e);
+    }
+  }
+}
---
+++ src/http/server/WebServer.java
@@ -0,0 +1,28 @@
+///A Simple Web Server (WebServer.java)
+
+package http.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+/**
+ * Example program from Chapter 1 Programming Spiders, Bots and Aggregators in
+ * Java Copyright 2001 by Jeff Heaton
+ * 
+ * WebServer is a very simple web-server. Any request is responded with a very
+ * simple web-page.
+ * 
+ * @author Jeff Heaton
+ * @version 1.0
+ */
+public class WebServer {
+
+  public static void main(String args[]) {
+   // WebServer ws = new WebServer();
+   // ws.start();
+  }
+
+}
